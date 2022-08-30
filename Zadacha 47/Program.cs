2b02e1949c05eaa08f4657acb7e38234a6036657
taron.poghosyan@yandex.ru <taron.poghosyan@yandex.ru>
2022-08-31 00:54:36 +0300

    Zadacha 47 resheno

--- Zadacha 47/Program.cs
+++ Zadacha 47/Program.cs
@@ -5,3 +5,37 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+
+double[,] CreateMatrixRndInt(int row, int col)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = Convert.ToDouble(rnd.Next(-100, 100)) / 10;
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(j < matrix.GetLength(1) ?
+            $"{matrix[i, j],3} " : $"{matrix[i, j],3}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] array2D = CreateMatrixRndInt(3, 4);
+PrintMatrix(array2D);
